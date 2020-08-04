5d6718d2609fbb224d8ebb740f7cf49fc73ce66f
Jorich P <w3bslave@gmail.com>
2020-08-04 20:34:58 +0800

    Create MyInfo.js

---
+++ components/MyInfo.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+function MyInfo() { //Basic React Functions
+  return (
+    <div>
+      <h1>Jorich Ponio/h1>
+      <p>This is a paragraph about me...</p>
+      <ul>
+        <li>Canada</li>
+        <li>Japan</li>
+        <li>Batanes Philippines</li>
+      </ul>
+    </div>
+  )
+}
+
+export default MyInfo
